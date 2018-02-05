b33dab9349f604a56cdae4bc3dd434feaa22e574
jiangguoxing <627344071@qq.com>
2018-02-05 15:41:10 +0800

    First commit

---
+++ src/SayHello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guoxing
+ * Date: 2018/2/5
+ * Time: 15:23
+ */
+
+namespace Composer;
+
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello world';
+    }
+}
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guoxing
+ * Date: 2018/2/5
+ * Time: 15:30
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+use Composer\SayHello;
+echo SayHello::world();
